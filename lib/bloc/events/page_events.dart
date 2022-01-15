3e92f18d3c54439a95dbca1583bf4693245dc044
Rashpinder Singh <dev.rashpinder@gmail.com>
2022-01-15 19:18:57 +0530

    Added the Apple Map Widget Added the Apple Polyline Page

---
+++ lib/bloc/events/page_events.dart
@@ -0,0 +1,5 @@
+abstract class PageEvents {}
+
+class PageLoadingEvents extends PageEvents {
+  PageLoadingEvents();
+}
--- lib/bloc/video_charts_bloc/video_charts_bloc.dart
+++ lib/bloc/video_charts_bloc/video_charts_bloc.dart
@@ -1,16 +1,17 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc_test_nextbase/bloc/events/page_events.dart';
+import 'package:flutter_bloc_test_nextbase/bloc/page_states/page_cubit_states.dart';
 import 'package:flutter_bloc_test_nextbase/core/network/result_error.dart';
 import 'package:flutter_bloc_test_nextbase/data_layer/data_models/video_meta_data_model.dart';
 import 'package:flutter_bloc_test_nextbase/data_layer/respository/video_meta_repo.dart';
 
-import '../page_cubit_states.dart';
-
-class VideoChartsCubit extends Cubit<PageCubitState> {
+class VideoChartsBloc extends Bloc<PageEvents, PageCubitState> {
   final VideoMetaDataRepository videoRepo;
   VideoMetaDataModel? videoMetaModel;
-  VideoChartsCubit(this.videoRepo) : super(PageCubitInitialState());
-
-  Future fetchVideoMetaData() async {
+  VideoChartsBloc(this.videoRepo) : super(PageCubitInitialState()) {
+    on<PageLoadingEvents>(_fetch);
+  }
+  void _fetch(PageLoadingEvents event, Emitter<PageCubitState> emit) async {
     final videoData = await videoRepo.getVideoMetaDataModel();
     if (videoData.data!.isNotEmpty) {
       videoMetaModel = videoData;
